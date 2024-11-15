05c7a8818ab0aa4c303b247c84e72bcd9d16f436
Umut ozkan <umuttozkannis@gmail.com>
2024-11-15 12:13:34 +0300

    test port

--- main.py
+++ main.py
@@ -66,5 +66,5 @@ def predict():
         return jsonify({'error': str(e)})
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))  # Default to 5000 if PORT is not set
+    port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port)
